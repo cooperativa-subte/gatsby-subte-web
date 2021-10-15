bc26ef813fa3618564e28b240236d760b89d0a96
Francisco Cobas <fcarocena@gmail.com>
2021-10-15 17:29:58 -0300

    Add more styles to contact page

--- src/@chakra-ui/gatsby-plugin/theme.js
+++ src/@chakra-ui/gatsby-plugin/theme.js
@@ -1,6 +1,10 @@
 import { extendTheme, theme } from '@chakra-ui/react';
 
 export default extendTheme({
+  fonts: {
+    heading: 'HelveticaMedium',
+    body: 'HelveticaLight',
+  },
   colors: {
     primary: theme.colors.black,
     secondary: 'white',
