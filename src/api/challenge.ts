7a56d7b835a2bc7194eb214313a00577c320c5e2
Francisco Cobas <fcarocena@gmail.com>
2022-03-28 09:56:20 -0300

    Fix API

--- src/api/challenge.ts
+++ src/api/challenge.ts
@@ -4,11 +4,11 @@ export default function handler(req: GatsbyFunctionRequest, res: GatsbyFunctionR
   if (req.method !== 'POST')
     return res.status(405).send({ message: 'This method is not available' });
 
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
   const body = JSON.parse(req.body);
 
-  if (!body.raiting) return res.status(400).send({ message: 'Param is missing' });
-
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  if (!body.raiting) return res.status(400).send({ message: 'Wrong param :(' });
 
   res.json({ message: 'Yey! Gracias por completar el desafío' });
 }
