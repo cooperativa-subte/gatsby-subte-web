b5e264edbd0fe7e20ae5561569088d31878e5e24
Francisco Cobas <fcarocena@gmail.com>
2022-06-01 18:04:52 -0300

    Try to remove errors from console

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,3 +1,9 @@
 import CustomLayout from './wrap-page-elements';
+// custom typefaces
+import 'typeface-montserrat';
+import 'typeface-merriweather';
+
+// custom CSS styles
+import './src/global.css';
 
 export const wrapPageElement = CustomLayout;
